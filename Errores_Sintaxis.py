16f5ee0b5f2c78e06e67a633999c2c41b909a815
Basilio Saldarriaga <basilio9210@gmail.com>
2019-07-15 08:07:03 -0500

    Create Errores_Sintaxis.py

---
+++ Errores_Sintaxis.py
@@ -0,0 +1,14 @@
+#Programa correctamente codificado:
+
+
+lado=int(input("Ingrese la medida del lado del cuadrado:"))
+superficie=lado*lado
+print("La superficie del cuadrado es")
+print(superficie)
+
+#Programa con un error sintáctico:
+
+lado=int(input("Ingrese la medida del lado del cuadrado:"))
+superficie=lado*lado
+print("La superficie del cuadrado es")
+print(Superficie)
